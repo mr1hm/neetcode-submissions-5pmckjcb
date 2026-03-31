029909559179a30f0f889f8b43eba9139759ec71
mr1hm <kevinihm@gmail.com>
2026-03-30 19:44:39 -0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+    seen := map[int]bool{}
+
+	for i := 0; i < len(nums); i++ {
+		if seen[nums[i]] {
+			return true
+		}
+
+		seen[nums[i]] = true
+	}
+
+	return false
+}
